5f6cd6a557e1a0cae3e938768fae0f6dd5a19688
chris <kiprutochris7@gmail.com>
2020-05-22 13:13:50 +0300

    added template layouts: back and front end

---
+++ resources/views/templates/back-layout/back.header.php
@@ -0,0 +1,28 @@
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <!-- bootstrap -->
+    <link rel="stylesheet" href="../../../public/css/bootstrap.min.css">
+    <!-- mdb  -->
+    <link rel="stylesheet" href="../../../public/css/mdb.min.css">
+    <!-- style  -->
+    <link rel="stylesheet" href="../../../public/css/style.css">
+    <!-- tailwindcss -->
+    <link rel="stylesheet" href="../../../src/build/tailwind.css">
+    <!-- datatables-->
+    <link rel="stylesheet" href="../../../public/css/addons/datatables.min.css">
+    <!-- fontawesome -->
+    <link rel="stylesheet" href="../../../public/fontawesome/css/all.min.css">
+    <!-- favicon -->
+    <link rel="shortcut icon" href="../../../public/img/icons/logo2.png" type="image/x-icon">
+</head>
+<body>
+
+    <header>
+
+    </header>
+
+    <main>
+
+        <!-- container section -->
---
+++ resources/views/templates/front-layout/app.footer.php
@@ -0,0 +1,19 @@
+    
+    </main>
+
+    <footer>
+        
+    </footer>
+
+    <!-- jquery -->
+    <script src="../../../public/js/jquery.min.js"></script>
+    <!-- popper -->
+    <script src="../../../public/js/popper.min.js"></script>
+    <!-- bootstrap -->
+    <script src="../../../public/js/bootstrap.min.js"></script>
+    <!-- mdb -->
+    <script src="../../../public/js/mdb.min.js"></script>
+    <!-- datatables -->
+    <script src="../../../public/js/addons/datatables.min.js"></script>
+</body>
+</html>
