b4c68d3ca8f53cf75875fccd61643be1d975faea
chris <kiprutochris7@gmail.com>
2020-05-22 13:21:32 +0300

    added app pages:home,about,services and services

---
+++ resources/views/pages/index.php
@@ -0,0 +1,8 @@
+<!-- app header -->
+<?php require '../templates/front-layout/app.header.php'; ?>
+
+<!-- title -->
+<title>Khan Store &bull; Home</title>
+
+<!-- app footer -->
+<?php require '../templates/front-layout/app.footer.php'; ?>
