6dff246e2f4afb8bf82b6b6f0ca44431694db5a0
chris <kiprutochris7@gmail.com>
2020-05-26 10:42:39 +0300

    modified footer and added about us title

--- resources/views/pages/about.php
+++ resources/views/pages/about.php
@@ -5,8 +5,14 @@
 <title>Khan Store &bull; About Us</title>
 
 <div class="container mt-20">
-    <div class="row">
-            
+    <div class="row d-flex justify-content-center">
+        <!-- company name -->
+        <div class="col-xl-6 col-lg-6 col-md-12 col-sm-12 mb-3">
+            <h4 class="text-gray-900 text-center text-2xl font-bold">
+                About 
+                <span class="red-text">Us</span>
+            </h4>
+        </div>
     </div>
 </div>
 
